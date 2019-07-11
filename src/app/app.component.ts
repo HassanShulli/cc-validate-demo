4ed93794a202ebbbe77267bd54d5175eeea7b2ab
Hassan Shulli <hassan14x@hotmail.com>
2019-07-12 00:46:08 +0800

    added cc-validate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {isValid} from 'cc-validate'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
+  currentNumber: any;
   currentStatus: any;
+  validateResponse: any;
 
   ngOnInit() {
+    this.currentNumber = '';
     this.currentStatus = 'VALID CREDIT CARD';
+  }
+
+  validate() {
+    this.validateResponse = isValid(this.currentNumber);
+    console.log('this.validateResponse : ', this.validateResponse);
+    if (this.validateResponse.isValid) {
+      this.currentStatus = 'VALID CREDIT CARD';
+    } else if (!this.validateResponse.isValid) {
+      this.currentStatus = 'INVALID CREDIT CARD';
+    }
+    this.currentNumber = this.validateResponse.cardNumber;
   }
 
 }
